c95cc8a96a76a8d2dc5715dcd21ef09af103a81b
Jayadeep Sasikumar <jayadeepsasikumar@gmail.com>
2023-12-04 14:25:23 +0000

    Refactoring command recognition code

--- speech/speech_recognition.py
+++ speech/speech_recognition.py
@@ -30,45 +30,19 @@ def speech_to_text_continuous(message_queue: Queue, api_key: str, speech_region:
     def recognized_speech(event: SpeechRecognitionEventArgs):
         logger.info(f"Recognized: {event.result.text}")
 
-        commands = ["up", "down", "left", "right", "jump", "stop", "step",
+        commands = ["up", "climb", "down", "town",
+                    "left", "write", "right", "jump", "stop", "step",
                     "turn", "done", "barry allen", "super lotto",]
         command_map = {
             "done": "turn",
+            "climb": "up",
+            "town": "down",
+            "write": "right",
         }
 
         for command in commands:
             if command in event.result.text.lower():
                 message_queue.put(command_map.get(command, command))
-
-        # if "up" in event.result.text.lower():
-        #     message_queue.put("up")
-
-        # if "down" in event.result.text.lower():
-        #     message_queue.put("down")
-
-        # if "left" in event.result.text.lower():
-        #     message_queue.put("left")
-
-        # if "right" in event.result.text.lower():
-        #     message_queue.put("right")
-
-        # if "jump" in event.result.text.lower():
-        #     message_queue.put("jump")
-
-        # if "stop" in event.result.text.lower():
-        #     message_queue.put("stop")
-
-        # if "step" in event.result.text.lower():
-        #     message_queue.put("step")
-
-        # if "turn" in event.result.text.lower() or "done" in event.result.text.lower():
-        #     message_queue.put("turn")
-
-        # if "barry allen" in event.result.text.lower():
-        #     message_queue.put("barry allen")
-
-        # if "super lotto" in event.result.text.lower():
-        #     message_queue.put("super lotto")
 
     # Init engine
     speech_config = speechsdk.SpeechConfig(subscription=api_key, region=speech_region)
@@ -87,6 +61,6 @@ def speech_to_text_continuous(message_queue: Queue, api_key: str, speech_region:
     speech_recognizer.canceled.connect(stop_cb)
 
     speech_recognizer.start_continuous_recognition()
-    
+
     while not done:
         time.sleep(.5)
