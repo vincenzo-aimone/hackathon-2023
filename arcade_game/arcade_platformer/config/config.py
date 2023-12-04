c22e75feb1436440f10c8a49742bae0862e846cb
Jayadeep Sasikumar <jayadeepsasikumar@gmail.com>
2023-12-04 15:51:22 +0000

    Modified platform margins

--- arcade_game/arcade_platformer/config/config.py
+++ arcade_game/arcade_platformer/config/config.py
@@ -5,18 +5,20 @@ import pathlib
 
 # Game constants
 
+NEW_SCALING = 1
+
 # Window dimensions
-SCREEN_WIDTH = 1000
-SCREEN_HEIGHT = 650
+SCREEN_WIDTH = int(1000 * NEW_SCALING)
+SCREEN_HEIGHT = int(650 * NEW_SCALING)
 SCREEN_TITLE = "Hackathon Arcade Game"
 
 # Scaling constants
-MAP_SCALING = 1.0
+MAP_SCALING = 1
 
 # Player constants
 GRAVITY = 1.0
-PLAYER_START_X = 65
-PLAYER_START_Y = 256
+PLAYER_START_X = 65 * NEW_SCALING
+PLAYER_START_Y = 256 * NEW_SCALING
 PLAYER_MOVE_SPEED = 20
 PLAYER_JUMP_SPEED = 25
 
@@ -24,10 +26,10 @@ PLAYER_JUMP_SPEED = 25
 # How close do we have to be to scroll the viewport?
 # Note the difference between LEFT_VIEWPORT_MARGIN and RIGHT_VIEWPORT_MARGIN.
 # This allows the alien to get closer to the left edge than the right.
-LEFT_VIEWPORT_MARGIN = 150
-RIGHT_VIEWPORT_MARGIN = 300
-TOP_VIEWPORT_MARGIN = 150
-BOTTOM_VIEWPORT_MARGIN = 150
+LEFT_VIEWPORT_MARGIN = int(50 * NEW_SCALING)
+RIGHT_VIEWPORT_MARGIN = int(850 * NEW_SCALING)
+TOP_VIEWPORT_MARGIN = int(300 * NEW_SCALING)
+BOTTOM_VIEWPORT_MARGIN = int(150 * NEW_SCALING)
 
 # Number of lives the player can use to win the game
 TOTAL_LIFE_COUNT = 5
@@ -37,10 +39,10 @@ ASSETS_PATH = pathlib.Path(__file__).resolve().parent.parent.parent / "assets"
 
 
 # Minimap config - Background color must include an alpha component
-MINIMAP_BACKGROUND_COLOR = arcade.get_four_byte_color((239, 222, 205, 80))
-MAP_WIDTH = {1: 3200, 2: 4800, 3: 6400, 4: 6400}
+MINIMAP_BACKGROUND_COLOR = arcade.get_four_byte_color((239, 222, 205, 150))
+MAP_WIDTH = {1: 4800, 2: 4800, 3: 6400, 4: 6400}
 MAP_HEIGHT = {1: 1024, 2: 1280, 3: 2560, 4: 2560}
-MINIMAP_WIDTH = {1: 192, 2: 288, 3: 384, 4: 384}
+MINIMAP_WIDTH = {1: 288, 2: 288, 3: 384, 4: 384}
 MINIMAP_HEIGHT = {1: 64, 2: 80, 3: 160, 4: 160}
 
 IDLE_COMMAND_TIME = 5
